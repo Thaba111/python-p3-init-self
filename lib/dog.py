3ea106139efa4c4807e08edc6456cbc466662922
Edith Thaba <writersherry5@gmail.com>
2024-03-05 20:41:56 +0300

    Done with assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+def test_self_name(self):
+    fido = Dog("Fido")
+    assert(fido.name == "Fido")
+
+def test_saves_self_breed(self):
+    fido = Dog("Fido", "Dalmatian")
+    assert(fido.breed == "Dalmatian")
+
+def test_default_breed(self):
+    fido = Dog("Fido")
+    assert(fido.breed == "Mutt")
+
+    
+
+    
